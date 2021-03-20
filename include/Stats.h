7b3e9df7a90aa8b8dc55f253e916607d2d311e3d
tkharisov7 <tkharisov7@gmail.com>
2021-03-20 19:08:59 +0300

    Added some cpp files and Stats.h

---
+++ include/Stats.h
@@ -0,0 +1,8 @@
+//
+// Created by Timur Kharisov on 20.03.2021.
+//
+
+#ifndef CONSOLE_GAME_STATS_H
+#define CONSOLE_GAME_STATS_H
+
+#endif //CONSOLE_GAME_STATS_H
---
+++ src/StatsFabric.cpp
@@ -0,0 +1,4 @@
+//
+// Created by Timur Kharisov on 20.03.2021.
+//
+
