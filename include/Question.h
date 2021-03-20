cdf172ecf51f3c74bff094b581741d359dec9f6e
mukhametgalin <mukhametgalin.ad@phystech.edu>
2021-03-20 18:41:16 +0300

    Question.h added

---
+++ include/Question.h
@@ -0,0 +1,26 @@
+//
+// Created by artur on 20.03.2021.
+//
+
+#ifndef CONSOLE_GAME_QUESTION_H
+#define CONSOLE_GAME_QUESTION_H
+
+class Question {
+//Fields:
+public:
+    std::string textOfQuestion;
+
+    //Impact of the question on Stats if answer is positive
+    std::vector<Stats> impactOnStatsPositive;
+    //Impact of the question on Stats if answer is negative
+    std::vector<Stats> impactOnStatsNegative;
+// Methods:
+public:
+    // Constructor
+    Question();
+    // Destructor
+    ~Question();
+};
+
+
+#endif //CONSOLE_GAME_QUESTION_H
