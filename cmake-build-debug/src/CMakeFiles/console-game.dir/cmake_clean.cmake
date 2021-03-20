85e7b51a279a74eff2d2054439d1a72251463fa2
tkharisov7 <tkharisov7@gmail.com>
2021-03-20 21:13:48 +0300

    deleted uncommon cmake

---
+++ cmake-build-debug/src/CMakeFiles/console-game.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "../../bin/console-game"
+  "../../bin/console-game.pdb"
+  "CMakeFiles/console-game.dir/Game.cpp.o"
+  "CMakeFiles/console-game.dir/Question.cpp.o"
+  "CMakeFiles/console-game.dir/QuestionFabric.cpp.o"
+  "CMakeFiles/console-game.dir/Stats.cpp.o"
+  "CMakeFiles/console-game.dir/StatsFabric.cpp.o"
+  "CMakeFiles/console-game.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/console-game.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
