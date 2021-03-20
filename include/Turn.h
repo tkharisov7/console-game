f7d5fdf16b50214380773b6e4ff50795167306eb
mukhametgalin <mukhametgalin.ad@phystech.edu>
2021-03-20 17:49:56 +0300

    some corrections and comments in Turn.h

--- include/Turn.h
+++ include/Turn.h
@@ -16,14 +16,21 @@ public:
 
 private:
 // Classes:
+    //we need this class to finish the game correctly if we lose
     class logFromRun;
+
 // Methods:
 public:
+    //starts the process of asking a question
     logFromRun run();
 private:
+    //gets question from the pull of questions
     void getQuestion();
+    // checks the question for correctness
     bool checker(const Question&);
 
+    //asks the question and gets an answer
     bool processQuestion(const Question&);
+    //processes result of question
     void processResultOfQuestion(bool);
 };
