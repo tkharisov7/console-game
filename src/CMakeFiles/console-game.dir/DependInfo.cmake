dd8f823d6bbbf492a72cbc5277542dc8b6d4d74c
tkharisov7 <tkharisov7@gmail.com>
2021-03-21 14:46:56 +0300

    Transfered from dev

---
+++ src/CMakeFiles/console-game.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/Game.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/Game.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/GameMode.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/GameMode.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/Question.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/Question.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/QuestionFabric.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/QuestionFabric.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/Stats.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/Stats.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/StatsFabric.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/StatsFabric.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/Turn.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/Turn.cpp.o"
+  "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/main.cpp" "/Users/timur_kharisov/OneDrive/Study/mipt/coding/Programming-Technologies-2-sem/console-game/src/CMakeFiles/console-game.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
